c9863a8e08035b37c5a43c47219db96c7a6bf3b4
Manveroo <cwdrspam-github@yahoo.de>
2017-11-18 17:35:28 +0100

    memory hog as function

--- app/memory/memory.js
+++ app/memory/memory.js
@@ -10,18 +10,22 @@
         // just generating some big object to have an impact when this controller is not freed
         var documentAsJSON = JSON.stringify(document);
         memory.bigObject = [];
-        for(var i=0; i<100000; i++) {
-            memory.bigObject.push({
-                index: i,
-                someNumber: Math.random(),
-                longString: documentAsJSON + i // making sure it doesn't reference the same string
-            });
-        }
+        captureSomeData(memory.bigObject);
 
         // leak
         $rootScope.$watch(UserService.getUserName, function(newVal) {
             memory.userName = newVal;
         });
+
+        function captureSomeData(targetArray) {
+            for(var i=0; i<100000; i++) {
+                targetArray.push({
+                    index: i,
+                    someNumber: Math.random(),
+                    longString: documentAsJSON + i // making sure it doesn't reference the same string
+                });
+            }
+        }
 
     });
 
